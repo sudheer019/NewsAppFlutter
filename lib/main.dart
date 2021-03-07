79001b6a82c3cd7de5ba7603293ad63e6cbb276e
NagaSudheer_G <sudheer.g@conceptglobal.com>
2021-03-07 15:14:33 +0530

    Chnaged App Name

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter'),
+      home: MyHomePage(title: 'NewsAppFlutter'),
     );
   }
 }
